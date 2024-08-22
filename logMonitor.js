c6b6f68776b0f46c1bb32216a396ada84773ffcb
Inesh-Reddy <ineshreddy007@gmail.com>
2024-08-22 20:06:43 +0530

    logging the data

--- logMonitor.js
+++ logMonitor.js
@@ -1,12 +1,9 @@
-const fs = require('fs');
+const fs = require("fs");
 
-console.log('message: log and monitoring system is up and running.....🏃🏃');
+console.log("message: log and monitoring system is up and running.....🏃🏃");
 
 let data;
-fs.readFile('./logs/logfile.txt', 'utf-8', function (_err, data) {
+fs.readFile("./logs/logfile.txt", "utf-8", function (_err, data) {
   console.log(data);
 });
-
-
-
-
+console.log("Successfully logged data.....");
